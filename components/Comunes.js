a396775cc078edbb6c7d9c4cf6dc4cf62855dd58
jogaro1983 <jogaro1983@hotmail.es>
2022-12-03 20:05:58 +0100

    Add files via upload

---
+++ components/Comunes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Comunes extends React.Component{
+
+    render(){
+        return (
+            <React.Fragment>
+                <p>Estas llamando a componente Comunes</p>
+            </React.Fragment>          
+        );
+    }
+
+}
+
+export default Comunes;
